d4f9fd8c5dce175b464c5b9b50865bdb11aad498
Mukul <mukujjee@gmail.com>
2024-03-11 18:23:50 +0530

    Themeing process started

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,13 +1,17 @@
 import { ContactButtons } from "@/components/contact-buttons";
 import { NavigationButtons } from "@/components/navbar";
 import { ModeToggle } from "@/components/providers/mode-toggle";
-import { Home } from "lucide-react";
+import Image from "next/image";
+import logo from "@/public/icononly_transparent_nobuffer.png";
+import Link from "next/link";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="px-8">
       <nav className="py-4 flex justify-between items-center">
-        <Home />
+        <Link href="/">
+          <Image width={60} height={60} src={logo} alt="logo" />
+        </Link>
         <NavigationButtons />
         <ModeToggle />
       </nav>
